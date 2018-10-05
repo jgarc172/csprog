7881d2e57b427fdc02b44dd3d81ea840c1ff7e80
pepe <jose@Administrators-MacBook-Pro.local>
2018-10-04 18:06:47 -0700

    finished second use case

---
+++ lab7-1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace lab7_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Association Relationship
+            Manager CEO = new Manager();
+            SwipeCard entry = new SwipeCard();
+            CEO.Logon(entry);
+                      
+            //Aggregation Relationship
+            Worker NewHire = new Worker();
+            NewHire.WorkerName = "Laura Croft";
+            CEO.Workers[0] = NewHire;
+            Console.WriteLine("worker {0}", CEO.Workers[0].WorkerName);
+
+            /*
+            //Composition Relationship
+            CEO.HowisTheManager(true);
+            Console.WriteLine(CEO.Salary);
+
+            Console.ReadLine();  
+            */
+        }
+    }
+    class Worker
+    {
+        public string WorkerName {set; get;}
+    }
+    class Manager
+    {
+        public Worker[] Workers = new Worker[10];
+        public void Logon(SwipeCard card)
+        {
+            card.Swipe(this);
+        }
+        public string GetManagerName()
+        {
+            return "Boss";
+        }
+    }
+    class SwipeCard
+    {
+        public void Swipe(Manager mgr)
+        {
+            Console.WriteLine("Welcome to XYZ, ");
+            Console.WriteLine(mgr.GetManagerName());
+        }
+    }
+}
