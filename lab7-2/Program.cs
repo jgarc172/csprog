59efbedbfa3608c55376d8355e05aa0712d96200
pepe <jose@Administrators-MacBook-Pro.local>
2018-10-07 14:42:25 -0700

    completed

---
+++ lab7-2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace lab7_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Manager CEO = new Manager();
+            Worker NewHire = new Worker();
+            NewHire.WorkerName = "Laura Croft";
+            CEO.Workers[0] = NewHire;
+        }
+    }
+    class Manager
+    {
+        public Worker[] Workers = new Worker[10];
+
+    }
+    class Worker
+    {
+        public string WorkerName {get; set;}
+    }
+}
