db4b16d78541badd631e296b06d82f46b1bd1087
pepe <jose@Administrators-MacBook-Pro.local>
2018-10-16 17:22:12 -0700

    before printCheck

---
+++ lab8-1/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace lab8_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "John Smith";
+            string ssn = "444-56-7777";
+            HourlyEmp newHire = new HourlyEmp(ref name, ref ssn, 65.50, 3);
+            newHire.setHours(45.45);
+            Console.Write($"New Employee: {newHire.getName()} \n {newHire.getSsn()} \n Hours: {newHire.getHours()} \n");
+            newHire.giveRaise(3.2);
+            newHire.setHours(40.0);
+            newHire.printCheck();
+        }
+    }
+    public class Employee
+    {
+        protected string name;
+        protected string ssn;
+        protected double netPay;
+        public Employee(ref string newName, ref string newSSN)
+        {
+            this.name = newName;
+            this.ssn = newSSN;
+        }
+        public string getName()
+        {
+            return this.name;
+        }
+        public string getSsn(){
+            return this.ssn;
+        }
+    }
+    public class HourlyEmp : Employee
+    {
+        private double payRate;
+        private double hours;
+        public HourlyEmp(ref string newName,
+                         ref string newSSN,
+                         double newPayRate,
+                         double newHours)
+            : base(ref newName, ref newSSN)
+        {
+            this.payRate = newPayRate;
+            this.hours = newHours;
+            this.netPay = this.hours * this.payRate;
+        }
+        public void setHours(double newHours){
+            this.hours = newHours;
+        }
+        public double getHours()
+        {
+            return this.hours;
+        }
+        public void giveRaise(double amount)
+        {
+            this.payRate += amount;
+            this.netPay = this.payRate * hours;
+        }
+    }
+}
