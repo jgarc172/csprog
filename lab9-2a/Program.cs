b1521ea49fb090ee4f2e6536c37967f3ceeb0447
pepe <jose@Administrators-MacBook-Pro.local>
2018-10-27 09:05:05 -0700

    completed lab9-2a

---
+++ lab9-2a/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace lab9_2a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DerivedClass1 B = new DerivedClass1();
+            B.DoWork();
+            BaseClass A = new BaseClass();
+            A = B;
+            A.DoWork();
+        }
+    }
+    class BaseClass
+    {
+        public virtual void DoWork()
+        {
+            Console.WriteLine("BaseClass DoWork");
+        }
+    }
+    class DerivedClass1 : BaseClass
+    {
+        public override void DoWork()
+        {
+            Console.WriteLine("DerivedClass1 DoWork");
+        }
+    }
+}
