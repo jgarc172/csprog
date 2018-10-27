8355cfb809d4394b8c7e517a068d49cb2175d36c
jgarc172 <jgarc172@mail.ccsf.edu>
2018-10-27 11:56:37 -0700

    completed

---
+++ lab9-3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace lab9_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Guy joe1 = new Guy("Joe", 37, 100);
+            Guy joe2 =  joe1;
+            Console.WriteLine(joe1);
+            Console.WriteLine(joe2);
+        }
+    }
+    class Guy
+    {
+        private readonly string name;
+        public string  Name {get {return name;}}
+        private readonly int age;
+        public int Age {get {return age;}}
+        public int Cash {get; private set;}
+        public Guy(string name, int age, int cash)
+        {
+            this.name = name;
+            this.age = age;
+            Cash = cash;
+        }
+        public override string ToString()
+        {
+            return $"{this.name} is {this.age} years old and has {Cash} bucks";
+        }
+    }
+}
