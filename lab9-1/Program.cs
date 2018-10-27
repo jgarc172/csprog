6f2bbe71f2e64f04c4c66d21ffabc633c4b988b0
pepe <jose@Administrators-MacBook-Pro.local>
2018-10-27 08:50:04 -0700

    completed lab9-1

---
+++ lab9-1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace lab9_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Shape[] shapes = new Shape[3];
+            shapes[0] = new Rectangle();
+            shapes[1] = new Triangle();
+            shapes[2] = new Circle();
+
+            foreach (var shape in shapes)
+            {
+                shape.Draw();
+            }
+        }
+    }
+
+    class Shape
+    {
+        public virtual void Draw()
+        {
+            Console.WriteLine("Shape Draw");
+        }
+    }
+    class Rectangle : Shape
+    {
+        public override void Draw()
+        {
+            base.Draw();
+            Console.WriteLine("Rectangle Draw");
+        }
+    }
+    class Triangle : Shape
+    {
+        public override void Draw(){
+            Console.WriteLine("Triangle Draw");
+        }
+    }
+    class Circle : Shape
+    {
+        public override void Draw(){
+            Console.WriteLine("Circle Draw");
+        }
+ 
+    }
+}
