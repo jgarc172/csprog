1f40a21c92cd7516f2bb1926febcfd23d162d26a
jgarc172 <jgarc172@mail.ccsf.edu>
2018-10-27 09:26:06 -0700

    completed

---
+++ lab9-2b/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace lab9_2a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DerivedClass2 C = new DerivedClass2();
+            C.DoWork();
+            BaseClass D = new BaseClass();
+            D = C;
+            D.DoWork();
+        }
+    }
+    class BaseClass
+    {
+        public int WorkField;
+        public virtual void DoWork()
+        {
+            Console.WriteLine("BaseClass DoWork");
+        }
+    }
+    class DerivedClass2 : BaseClass
+    {
+        public new int WorkField;
+        public new void DoWork()
+        {
+            base.DoWork();
+            WorkField++;
+            Console.WriteLine("DerivedClass2 DoWork");
+        }
+    }
+}
