08e8c18a4fe0461de1d82aada7989b9ed3483d59
jgarc172 <jgarc172@mail.ccsf.edu>
2018-11-09 12:05:56 -0800

    completed lab11-2

---
+++ lab11-2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace lab11_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle cir = new Circle();
+            cir.Draw();
+            Console.WriteLine(cir.X);
+            Console.WriteLine(cir.Y);
+        }
+    }
+
+    public abstract class Shape
+    {
+        protected int _x = 100;
+        protected int _y = 150;
+        public abstract int X { get; }
+        public abstract int Y { get; }
+        public abstract void Draw();
+    }
+    class Circle : Shape
+    {
+        public override int X
+        {
+            get
+            {
+                return _x + 10;
+            }
+        }
+        public override int Y
+        {
+            get
+            {
+                return _y + 10;
+            }
+        }
+        public override void Draw()
+        {
+            Console.WriteLine("Drawing a Circle");
+        }
+    }
+}
