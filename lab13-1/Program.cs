ba66e6e70a8f37cea200b009c6ca98e63f7979d4
jgarc172 <jgarc172@mail.ccsf.edu>
2018-11-12 10:24:46 -0800

    finished lab13-1

---
+++ lab13-1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace lab13_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.Write("Input a number between 0 and 5 " +
+                       "(or just hit return to exit)> ");
+                    string userInput = Console.ReadLine();
+                    if (userInput == "")
+                        break;
+                    int index = Convert.ToInt32(userInput);
+                    if (index < 0 || index > 5)
+                        throw new IndexOutOfRangeException("You typed in " + userInput);
+                    Console.WriteLine("Your number was " + index);
+                }
+                catch (IndexOutOfRangeException e)
+                {
+                    Console.WriteLine("Exception: " +
+                       "Number should be between 0 and 5. " + e.Message);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(
+                       "An exception was thrown. Message was: " + e.Message);
+                }
+                finally
+                {
+                    Console.WriteLine("Thank you");
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
