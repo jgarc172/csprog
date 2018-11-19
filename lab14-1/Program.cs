42b0277cfe0ba749e08a2641977072b88b4e23ef
jgarc172 <jgarc172@mail.ccsf.edu>
2018-11-18 18:53:38 -0800

    completed lab14-1

---
+++ lab14-1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+
+namespace lab14_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProcessText addressBook = new ProcessText();
+            Console.Write("Enter file to read from: ");
+            String inFile = Console.ReadLine();
+            Console.Write("Enter file to write to: ");
+            String outFile = Console.ReadLine();
+            addressBook.openFile(inFile, outFile, "415", "628");
+        }
+
+    }
+    class ProcessText
+    {
+        public void openFile(String source, String target, String oldStr, String newStr)
+        {
+            StreamReader reader = new StreamReader(new FileStream(source, FileMode.Open));
+            StreamWriter writer = new StreamWriter(new FileStream(target, FileMode.Create));
+            String line1;
+            String line2;
+            while((line1 = reader.ReadLine()) != null)
+            {
+                line2 = line1.Replace(oldStr, newStr);
+                writer.WriteLine(line2);
+            }
+
+            reader.Close();
+            writer.Close();
+        }
+    }
+}
