1268a188b3ca22276410c36e97b7acdd9f83419e
jgarc172 <jgarc172@mail.ccsf.edu>
2018-11-19 22:46:11 -0800

    started lab14-2

---
+++ lab14-2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace lab14_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string prompt = "Please type in the name of the file > "; 
+            Console.Write(prompt);
+            string fileName = Console.ReadLine();
+            ColdCallFileReader peopleToRing = new ColdCallFileReader();
+
+            try
+            {
+                peopleToRing.Open(fileName);
+            }
+            catch (FileNotFoundException)
+            {
+                
+            
+            }
+            finally
+            {
+
+            }
+        }
+    }
+    class ColdCallFileReader
+    {
+        public void Open(string fileName)
+        {
+            
+        }
+    }
+}
