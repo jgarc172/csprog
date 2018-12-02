a283b3cbb740348ce309fb913508524e89b3c376
jgarc172 <jgarc172@mail.ccsf.edu>
2018-12-02 12:52:18 -0800

    finished lab15-4

---
+++ lab15-4/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lab15_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<char> q = new Queue<char>();
+            q.Enqueue('a');
+            q.Enqueue('b');
+            q.Enqueue('c');
+            q.Enqueue('d');
+
+            Console.WriteLine("queue contains ");
+            printQ(q);
+
+            q.Enqueue('a');
+            q.Enqueue('b');
+            q.Enqueue('c');
+            q.Enqueue('d');
+
+            Stack<char> iStack = new Stack<char>();
+            Console.WriteLine("reversing queue . . .");
+            while (q.Count > 0)
+            {
+                iStack.Push(q.Dequeue());
+            }
+            while (iStack.Count > 0)
+
+            {
+
+                q.Enqueue(iStack.Pop());
+
+                Console.WriteLine(q.Dequeue());
+
+            }
+
+        }
+        public static void printQ(Queue<char> q)
+        {
+            while (q.Count > 0)
+            {
+                Console.WriteLine(q.Dequeue());
+            }
+        }
+
+    }
+}
