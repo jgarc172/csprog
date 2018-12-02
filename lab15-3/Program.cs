a2430673563efcc925f4e502bb2b2c3723a3dd82
jgarc172 <jgarc172@mail.ccsf.edu>
2018-12-02 12:12:09 -0800

    completed lab15-3

---
+++ lab15-3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lab15_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<char> alphabet = new Stack<char>();
+            alphabet.Push('A');
+            Console.WriteLine(alphabet.Peek());
+            
+            alphabet.Push('B');
+            Console.WriteLine(alphabet.Peek());
+            
+            alphabet.Push('C');
+            Console.WriteLine(alphabet.Peek());
+
+            Console.WriteLine("First iteration:");
+            while (alphabet.Count > 0)
+            {
+                Console.WriteLine(alphabet.Pop());
+            }
+
+            Console.WriteLine("count is {0}", alphabet.Count);
+        }
+    }
+}
